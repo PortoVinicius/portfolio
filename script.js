ede4f7f7e8f3807d8b88d984769fef3a753a75c4
Vinicius <141542477+PortoVinicius@users.noreply.github.com>
2025-11-15 02:12:18 -0300

    Refactor addComment function for input validation
    Updated the addComment function to validate input and redirect to a new page.

--- script.js
+++ script.js
@@ -1,13 +1,11 @@
 function addComment() {
-    const input = document.getElementById("commentInput");
-    const commentsDiv = document.getElementById("comments");
+    const comment = document.getElementById("commentInput").value;
 
-    if (input.value.trim() === "") return;
+    if (comment.trim() === "") {
+        alert("Digite algo no comentário.");
+        return;
+    }
 
-    const newComment = document.createElement("div");
-    newComment.classList.add("comment");
-    newComment.innerText = input.value;
-
-    commentsDiv.prepend(newComment); // aparece primeiro
-    input.value = "";
+    // Abre a página do GIF
+    window.location.href = "depoimento.html";
 }
